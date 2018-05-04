46c1c16ee066d5ef95cd15d2a5a8201b762c802d
Matthew Ellis <matt.ellis012@gmail.com>
2018-05-04 16:19:34 -0400

    Initial 'Primitive' commit Using taniarascia's 'primitive' Sass boilerplate for some basic styling and using a gulp workflow to compile .scss files to css

---
+++ Gulpfile.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var cssnano = require('gulp-cssnano');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+var rename = require('gulp-rename');
+
+gulp.task('workflow', function () {
+    gulp.src('./blog/src/sass/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('./blog/static/css/')) // unminified
+        .pipe(cssnano({
+            zindex: false
+        }))
+        .pipe(sourcemaps.write('./'))
+        .pipe(rename(function (path) {
+            if(path.extname === '.css') {
+                path.basename += '.min';
+            }
+        }))
+        .pipe(gulp.dest('./blog/static/css/'))
+        // .pipe(gulp.dest('./docs/css/')) // comment out for production
+});
+
+//Watch task
+gulp.task('default', function () {
+  gulp.watch('./blog/src/sass/**/*.scss', ['workflow']);
+});
